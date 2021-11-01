421b18ccfe77e220b76a768a641a786564a93a34
NathasyaSR <65170015+nthswny@users.noreply.github.com>
2021-11-01 17:13:20 +0700

    Create home.dart

---
+++ home.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'card1.dart';
+import 'card2.dart';
+import 'card3.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  //TODO: Add state variables and functions
+  int _selectedIndex = 0;
+  static List<Widget> pages = <Widget>[
+    //TODO: Replace with Card1
+    const Card1(),
+    //TODO: Replace with Card2
+    const Card2(),
+    //TODO: Replace with Card3
+    const Card3(),
+  ];
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Fooderlich',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+      ),
+      //TODO: Show selected tab
+      body: pages[_selectedIndex],
+      //TODO: Add bottom navigation bar
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        //TODO: Set selected tab bar
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: <BottomNavigationBarItem>[
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card2',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card3',
+          ),
+        ],
+      ),
+    );
+  }
+}
